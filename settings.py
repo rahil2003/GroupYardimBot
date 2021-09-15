242db1b6faf10dc6573e71eb92581af79352b0b4
rahil2003 <85434308+rahil2003@users.noreply.github.com>
2021-09-15 04:32:33 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-1944354228:AAGxo9SPPBy_u5c2-78iRIFSxLBJ61bM3vo = "123456789876:Abkkanbsbaks"
+1944354228:AAGxo9SPPBy_u5c2-78iRIFSxLBJ61bM3vo = 
 with open('words.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
